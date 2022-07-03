b0f9d2b0ead54c2b32c512eeffe2be89bbc832ef
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-03 01:56:43 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const helemt = require("helmet");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const MongooseConnection = require("./utility/mongoose.connection");
 const cors = require("cors");
@@ -7,6 +9,11 @@ require("dotenv").config();
 
 MongooseConnection();
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(helemt());
+app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(cors());
 
